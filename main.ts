e99256140894fb168fb2b298cda6c387cf926473
okioat <118501788+okioat@users.noreply.github.com>
2022-11-27 18:35:02 +0700

    change speed 100%

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 50)
+    TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 100)
     if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 15)) {
         TPBot.setTravelTime(TPBot.DriveDirection.Backward, 50, 1)
         TPBot.setTravelTime(TPBot.DriveDirection.Left, 50, 0.5)
